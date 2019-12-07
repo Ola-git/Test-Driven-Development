9785cad184e217b3828e12a44cc7bd6757863cde
szymon-git <szymon-git@users.noreply.github.com>
2019-12-07 14:26:24 +0100

    feat: add program to test

--- TestStrings.py
+++ TestStrings.py
@@ -1,23 +1,23 @@
 import unittest
 import program
 
-class Test_Str_methods():
+class Test_Str_methods(unittest.TestCase):
 
     def test_upper(self):
         self.assertEqual(program.doUpper("ala ma kota"), "ALA MA KOTA")
 
-    def test_capitalise(self):
-        self.assertEqual(program.doCapitalise("ola"), "Ola")
+    def test_capitalize(self):
+        self.assertEqual(program.doCapitalize("ola"), "Ola")
 
     def test_replace(self):
-        str = program.doReplace("co", "na co")
-        self.assertTrue(str.index("na co") > 0)
+        str = program.doReplace("bananas", "apples", "I like bananas")
+        self.assertTrue(str.index("apples") > 0)
 
     def test_lower(self):
         self.assertEqual(program.doLower("QWERTY"), "qwerty")
 
     def test_startswith(self):
-        self.assertEqual(program.doStartswith("Ala ma kota"), "Ala")
+        self.assertTrue(program.doStartswith("Ala ma kota"), "Ala")
 
 if __name__=="__main__":  #to trzeba zapamietac#
     unittest.main()
