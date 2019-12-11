57a2680880ad417a5c4354dc7de1115925fb02fa
Ola-Git <Ola-Git@users.noreplay.github.com>
2019-12-11 12:28:26 +0000

    fix: add confition "."

--- email_address.py
+++ email_address.py
@@ -1,7 +1,14 @@
-def is_correct(str):
+def is_correct1(str):
     str1 = str.split("@")[0]
     str2 = str.split("@")[1]
 
     if str == str1 + "@" + str2:
+        return True
+
+
+def is_correct2(str):
+    str2 = str.split("@")[1]
+
+    if "." in str2:
         return True
 
--- test_email_address.py
+++ test_email_address.py
@@ -4,5 +4,8 @@ import email_address
 
 class TestEmailAddress(unittest.TestCase):
 
-    def test_is_correct(self):
-        self.assertTrue(email_address.is_correct("olapyt@gmail.com"), "olapyt" + "@" + "gmail.com")
+    def test_is_correct1(self):
+        self.assertTrue(email_address.is_correct1("olapyt@gmail.com"), "olapyt" + "@" + "gmail.com")
+
+    def test_is_correct2(self):
+        self.assertTrue(email_address.is_correct2("olapyt@gmail.com"), ".")
